a541e0dd0ed5efd6def7b659ac370ab457ef048c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-19 03:22:28 +0600

    added Header and Footer components

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main></Main>,
+        children:[
+            {
+                path: "/",
+                element: <Main></Main>
+            }
+        ]
     },
 ]);
 
---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Copyright issue 2023</h2>
+        </div>
+    );
+};
+
+export default Footer;
