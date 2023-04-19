f2d02f0a53d3e3586d00b5a8f8105493cc26f0bc
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-19 13:47:59 +0600

    login button added in right nav

--- src/Shared/RightNav/RightNav.jsx
+++ src/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaGoogle , FaGithub } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            <h2>This is Right Nav</h2>
+            <h5 className='fw-bold mb-4'>Login With</h5>
+          
+            <Button className='mb-3' variant="outline-primary"> <FaGoogle className='me-3'/>Login With Google</Button>
+            <Button className='mb-3' variant="outline-primary"> <FaGithub className='me-3'/>Login With Github</Button>
+ 
         </div>
     );
 };
