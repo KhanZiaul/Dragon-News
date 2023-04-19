e4bfe528c43fa9c78a015fdca03ea7e351a21441
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-19 14:17:24 +0600

    card added

--- src/Shared/RightNav/RightNav.jsx
+++ src/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
-import { FaGoogle, FaGithub , FaFacebookF , FaTwitter , FaInstagram} from 'react-icons/fa';
+import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 import qZone1 from '../../assets/qZone1.png'
 import qZone2 from '../../assets/qZone2.png'
 import qZone3 from '../../assets/qZone3.png'
+import bg1 from '../../assets/bg1.png'
+import Card from 'react-bootstrap/Card';
 
 
 const RightNav = () => {
@@ -23,6 +25,18 @@ const RightNav = () => {
                 <img src={qZone1} alt="" />
                 <img src={qZone2} alt="" />
                 <img src={qZone3} alt="" />
+            </div>
+            <div>
+                <Card className="text-center text-white ">
+                    <Card.Img src={bg1} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title className='fs-2 fw-bold mt-5 mb-3'>Create an Amazing Newspaper</Card.Title>
+                        <Card.Text className='mb-3'>
+                            Discover thousands of options, easy to customize layouts, one-click to import demo and much more.
+                        </Card.Text>
+                        <Card.Text><Button variant="danger">Learn More</Button></Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
             </div>
         </div>
     );
