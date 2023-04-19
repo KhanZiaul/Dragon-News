e4366afa94404097b2e8350a63f4b64d7bc7135e
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-19 14:05:30 +0600

    Q-Zone added

--- src/Shared/RightNav/RightNav.jsx
+++ src/Shared/RightNav/RightNav.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 import { FaGoogle, FaGithub , FaFacebookF , FaTwitter , FaInstagram} from 'react-icons/fa';
+import qZone1 from '../../assets/qZone1.png'
+import qZone2 from '../../assets/qZone2.png'
+import qZone3 from '../../assets/qZone3.png'
 
 
 const RightNav = () => {
@@ -15,6 +18,12 @@ const RightNav = () => {
                 <ListGroup.Item className='mb-3'> <FaTwitter className='me-3' /> Twitter</ListGroup.Item>
                 <ListGroup.Item className='mb-3'> <FaInstagram className='me-3' /> Instagram</ListGroup.Item>
             </ListGroup>
+            <h3 className='my-4'>Q - Zone</h3>
+            <div>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
         </div>
     );
 };
