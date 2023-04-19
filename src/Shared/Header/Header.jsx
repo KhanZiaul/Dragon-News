beb03e5a32c1f31213d9d01736584a5e02690669
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-19 13:03:25 +0600

    Header button and moment.js package addded and applied

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
 import moment from 'moment';
+import { Button } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-         <img src={logo} alt="" />
-         <p>Journalism Without Fear or Favour</p>
-         <p>{moment().format()}</p>
+        <div className='container'>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p className='my-2'>Journalism Without Fear or Favour</p>
+                <p className='fs-4 fw-normal'>{moment().format("dddd, MMMM D , YYYY")}</p>
+            </div>
+            <div className='my-5'>
+                <Button variant="danger">Latest</Button>
+            </div>
         </div>
     );
 };
