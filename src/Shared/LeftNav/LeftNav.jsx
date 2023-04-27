7e59e3c9ba2c7ba1b73d06a3867dd4a80779c25c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-27 20:27:17 +0600

    my datas added

--- src/Shared/LeftNav/LeftNav.jsx
+++ src/Shared/LeftNav/LeftNav.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import './LeftNav.css'
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
-    const[categories,setCategories] = useState([])
+    const [categories, setCategories] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:3000/datas')
-        .then(res => res.json())
-        .then(datas => setCategories(datas))
-        .catch(error => console.error(error))
-    },[])
+            .then(res => res.json())
+            .then(datas => setCategories(datas))
+            .catch(error => console.error(error))
+    }, [])
     return (
         <div>
-            <h2>All Category</h2>
-            {
-                categories.map(categoriy => <p className='categoriy p-2'>{categoriy.name}</p>)
-            }
+            <h2 className='mb-3'>All Category</h2>
+            <div>
+                {
+                    categories.map(categoriy => <p><Link to={`categoriy/${categoriy.id}`} key={categoriy.id} className='categoriy p-2 text-decoration-none text-black mb-2'>{categoriy.name}</Link> </p> )
+                }
+            </div>
         </div>
     );
 };
