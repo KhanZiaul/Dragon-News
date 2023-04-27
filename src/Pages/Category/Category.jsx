7c3e9a6de7ed89bb7c6528455f7b57e90dbc7bf7
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-28 03:05:11 +0600

    added total view

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import { FaEye } from 'react-icons/fa';
+
 
 const Category = ({ category }) => {
-    const { image_url, title, details } = category
+    const { image_url, title, details, total_view } = category
     return (
-        <Card>
+        <Card className='my-5'>
             <Card.Header>Featured</Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
-                <Card.Img variant="top" src={image_url} />
+                <Card.Img className='my-4' variant="top" src={image_url} />
                 <Card.Text>
-                    {details.length < 250 ? details : <>{details.slice(0,250)}... <Link className='text-decoration-none text-danger fw-bold'>Read more</Link> </>}
+                    {details.length < 250 ? details : <>{details.slice(0, 250)}... <Link className='text-decoration-none text-danger fw-bold'>Read more</Link> </>}
                 </Card.Text>
             </Card.Body>
-            <Card.Header>Featured</Card.Header>
+            <Card.Header className='d-flex justify-content-between px-3 align-items-center'>
+                <div>
+
+                </div>
+                <div className='d-flex align-items-center gap-2 fw-bold'>
+                    <p> <FaEye /> </p>
+                    <p>{total_view}</p>
+                </div>
+            </Card.Header>
         </Card>
     );
 };
