50960400a03e8abc07baec9283ea28990efbc39d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-28 02:54:28 +0600

    added category title and details

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 
-const Category = ({category}) => {
-    const {image_url} = category
+const Category = ({ category }) => {
+    const { image_url, title, details } = category
     return (
         <Card>
             <Card.Header>Featured</Card.Header>
             <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
+                <Card.Title>{title}</Card.Title>
                 <Card.Img variant="top" src={image_url} />
-                
                 <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    {details.length < 250 ? details : <>{details.slice(0,250)}... <Link className='text-decoration-none text-danger fw-bold'>Read more</Link> </>}
                 </Card.Text>
             </Card.Body>
             <Card.Header>Featured</Card.Header>
