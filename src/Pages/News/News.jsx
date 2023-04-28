57a15518942deccc55c8dc39f937c07497fa35b3
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-28 12:29:05 +0600

    News details , image and title done

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { useLoaderData } from 'react-router-dom';
 
 const News = () => {
+    const news = useLoaderData()
+    const { image_url, title, details, total_view, author, rating, _id } = news
     return (
         <div>
-            <h2>News</h2>
+            <h2>Dragon News</h2>
+
+            <Card className='my-5'>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Img className='my-4' variant="top" src={image_url} />
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
         </div>
     );
 };
