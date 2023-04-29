ab2d40fd52072f1a98611efdb64fcf3280c58a5c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-29 11:21:25 +0600

    user profile added

--- src/Shared/NavigationVar/NavigationVar.jsx
+++ src/Shared/NavigationVar/NavigationVar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
+import './NavigationVar.css'
+import { FaRegUser } from 'react-icons/fa';
 
 const NavigationVar = () => {
+    const {user} = useContext(AuthContext)
     return (
         <Container>
             <Navbar bg="light" expand="lg">
@@ -22,7 +27,9 @@ const NavigationVar = () => {
                             <NavLink className='text-decoration-none text-black fw-bold' href="#">career</NavLink>
                         </Nav>
                         <div className="d-flex gap-3 align-items-center">
-                            <p>Profile</p>
+                            {
+                                user ? <img className='profile rounded-circle' src={user.photoURL} alt="" /> : <FaRegUser/>
+                            }
                             <Link to='/login'><Button variant="dark">Login</Button></Link>
                         </div>
                     </Navbar.Collapse>
